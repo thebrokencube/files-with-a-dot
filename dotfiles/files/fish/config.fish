ee6e8530b8488b957e9405e1b63ccd31b12cc9ae
Sapan Upadhyay <sapan.upadhyay@gmail.com>
2022-11-11 14:34:31 -0700

    shells: fix paths and cleanup configs to work on fresh machine

--- dotfiles/files/fish/config.fish
+++ dotfiles/files/fish/config.fish
@@ -1,7 +1,21 @@
 if status is-interactive
   alias vim=nvim
 
+  # Add dir for where homebrew applications get installed to if exists
+  if test -e /opt/homebrew/bin/brew
+    fish_add_path /opt/homebrew/bin
+  end
+
+  # Add /usr/local dirs to path if exist
+  if test -e /usr/local/bin
+    fish_add_path /usr/local/bin
+  end
+  if test -e /usr/local/sbin
+    fish_add_path /usr/local/sbin
+  end
+
   # environment variables for OOpinion development
-  set -x NODE_AUTH_TOKEN $(pass show OOPinion/NODE_AUTH_TOKEN)
+  # TODO: uncomment after pass setup
+  #set -x NODE_AUTH_TOKEN $(pass show OOPinion/NODE_AUTH_TOKEN)
   set -x VITE_API_URL http://localhost:3000
 end
