7f3ad9865438bdf7157689b020f4d5f237ce4b2f
Sapan Upadhyay <sapan.upadhyay@gmail.com>
2022-11-12 15:17:56 -0700

    shell config: add pass-backed ENV vars back in

--- dotfiles/files/fish/config.fish
+++ dotfiles/files/fish/config.fish
@@ -15,7 +15,6 @@ if status is-interactive
   end
 
   # environment variables for OOpinion development
-  # TODO: uncomment after pass setup
-  #set -x NODE_AUTH_TOKEN $(pass show OOPinion/NODE_AUTH_TOKEN)
+  set -x NODE_AUTH_TOKEN $(pass show files-with-a-dot/oopinion/NODE_AUTH_TOKEN)
   set -x VITE_API_URL http://localhost:3000
 end
