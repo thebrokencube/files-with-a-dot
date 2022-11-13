bb7638e0cdf166614efb0e70cdc8b6ce49246e5f
Sapan Upadhyay <sapan.upadhyay@gmail.com>
2022-11-13 09:05:30 -0700

    nvm: remove node/yarn from Brewfile, consolidate path autoload into ~/.profile

--- dotfiles/files/fish/config.fish
+++ dotfiles/files/fish/config.fish
@@ -1,18 +1,7 @@
 if status is-interactive
   alias vim=nvim
 
-  # Add dir for where homebrew applications get installed to if exists
-  if test -e /opt/homebrew/bin/brew
-    fish_add_path /opt/homebrew/bin
-  end
-
-  # Add /usr/local dirs to path if exist
-  if test -e /usr/local/bin
-    fish_add_path /usr/local/bin
-  end
-  if test -e /usr/local/sbin
-    fish_add_path /usr/local/sbin
-  end
+  bass source ~/.profile
 
   # environment variables for OOpinion development
   set -x NODE_AUTH_TOKEN $(pass show files-with-a-dot/oopinion/NODE_AUTH_TOKEN)
