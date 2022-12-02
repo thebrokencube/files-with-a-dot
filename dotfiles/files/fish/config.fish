fa79d8fdaaffc18b8aa84a1bd2d983ec35be20b0
Sapan Upadhyay <sapan.upadhyay@gmail.com>
2022-12-01 21:07:42 -0700

    fish config: add firebase credentials from pass

--- dotfiles/files/fish/config.fish
+++ dotfiles/files/fish/config.fish
@@ -10,4 +10,6 @@ if status is-interactive
   # environment variables for OOpinion development
   set -x NODE_AUTH_TOKEN $(pass show files-with-a-dot/oopinion/NODE_AUTH_TOKEN)
   set -x VITE_API_URL http://localhost:3000
+  set -x PASS_FIREBASE_PRIVATE_KEY $(pass show files-with-a-dot/hobby-honer/FIREBASE_PRIVATE_KEY)
+  set -x PASS_FIREBASE_CLIENT_EMAIL $(pass show files-with-a-dot/hobby-honer/FIREBASE_CLIENT_EMAIL)
 end
